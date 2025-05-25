3fc01d5d7dfd65e5e490533689aa9799c4016ad6
Nishant Sharma <nishantsharma7716@gmail.com>
2025-05-26 00:26:08 +0530

    update styling

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,61 +3,61 @@ import './about.css'
 function About(){
     return(
         <>
-        <div className="h flex flex-col items-center bg-slate-950 text-white" id="about">
+        <div className="h flex flex-col items-center bg-slate-200 dark:bg-slate-950 text-white" id="about">
             <p className='text-[2.5em] font-bold text-violet-600 mt-14'>About Me</p>
-            <p className='w-[75%] mt-8 text-[1.2em] text-white/80 text-center'>Hey there! 👋 I'm <span className='text-blue-600 font-semibold'>Nishant Sharma</span>, a Full-Stack Developer and final-year Computer Science Engineering student. I love transforming ideas into scalable web applications and solving complex coding challenges. My journey in tech revolves around continuous learning, experimenting with new technologies, and building solutions that make an impact.</p>
-            <div className='bg-blue-600  mt-7 sm:text-[1.1em] p-2 w-fit max-w-[80%] min-w-[10rem] rounded-md text-center text-white/80'>🚀 I like to build Products and solve problems</div>
+            <p className='w-[75%] mt-8 text-[1.2em] text-black/70 dark:text-white/80 text-center'>Hey there! 👋 I'm <span className='text-blue-600 font-semibold'>Nishant Sharma</span>, a Full-Stack Developer and final-year Computer Science Engineering student. I love transforming ideas into scalable web applications and solving complex coding challenges. My journey in tech revolves around continuous learning, experimenting with new technologies, and building solutions that make an impact.</p>
+            <div className='bg-blue-200/50 dark:bg-blue-600  mt-7 sm:text-[1.1em] p-2 w-fit max-w-[80%] min-w-[10rem] rounded-md text-center text-blue-600 dark:text-white/80'>🚀 I like to build Products and solve problems</div>
 
             <div className=' w-[100%] flex flex-col md:flex-row md:flex-wrap justify-around items-center  mt-10'>
-                <div className=' w-[90%] md:w-[45%]  mt-5 rounded-lg bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(37,_99,_235)]'>
+                <div className=' w-[90%] md:w-[45%]  mt-5 rounded-lg bg-white dark:bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(37,_99,_235)]'>
                     {/* inner div */}
                     <div className='flex items-center mt-5 pl-5 '>
                         <p className='h-[3rem] w-[3.2rem] bg-blue-700 flex justify-center items-center rounded-md'><i className="fa-regular fa-user text-[1.7em]"></i></p>
-                        <span className='text-[1.4em] pl-4 text-white/90 font-semibold'>Who I Am</span>
+                        <span className='text-[1.4em] pl-4 text-black dark:text-white/90 font-semibold'>Who I Am</span>
                     </div>
                     {/* list */}
-                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-white/50 '>
+                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-black/70 dark:text-white/50 '>
                     <li className='pb-1'>A full-stack developer having interest in software engineering.</li>
                     <li className='pb-1'>Enjoy solving problems and building scalable applications.</li>
                     <li className='pb-1'>Always learning new technologies to improve my skills.</li>
                     </p>
                 </div>
-                <div className=' w-[90%] md:w-[45%] mt-5 rounded-lg bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(109,_40,_217)]'>
+                <div className=' w-[90%] md:w-[45%] mt-5 rounded-lg bg-white dark:bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(109,_40,_217)]'>
                      {/* inner div */}
                      <div className='flex items-center mt-5 pl-5'>
                         <p className='h-[3rem] w-[3.2rem] bg-violet-700 flex justify-center items-center rounded-md'><i className="fa-solid fa-code text-[1.5em]"></i></p>
-                        <span className='text-[1.4em] pl-4 text-white/90 font-semibold'>Who I Do</span>
+                        <span className='text-[1.4em] pl-4 text-black dark:text-white/90 font-semibold'>Who I Do</span>
                     </div>
                     {/* list */}
-                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-white/50'>
+                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-black/70 dark:text-white/50'>
                     <li className='pb-1'>Develop high-performance web apps using modern tech stacks.</li>
                     <li className='pb-1'>Solve algorithmic problems and optimize code efficiency.</li>
                     <li className='pb-1'>Contribute to open-source and follow industry trends.</li>
                     </p>
                 </div>
-                <div className=' w-[90%] md:w-[45%] mt-5 md:mb-5  rounded-lg bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(21,_128,_61)]'>
+                <div className=' w-[90%] md:w-[45%] mt-5 md:mb-5  rounded-lg bg-white dark:bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(21,_128,_61)]'>
                      {/* inner div */}
                      <div className='flex items-center mt-5 pl-5 '>
                         <p className='h-[3rem] w-[3.2rem] bg-green-700 flex justify-center items-center rounded-md text-white'><i className="fa-solid fa-bullseye text-[1.7em]"></i></p>
-                        <span className='text-[1.4em] pl-4 text-white/90 font-semibold'>My Goals</span>
+                        <span className='text-[1.4em] pl-4 text-black dark:text-white/90 font-semibold'>My Goals</span>
                     
                     </div>
                     {/* list */}
-                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-white/50 '>
+                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-black/70 dark:text-white/50 '>
                     <li className='pb-1'>Build tech products that solve real-world challenges at scale.</li>
                     <li className='pb-1'>Advance as a full-stack developer with modern frameworks</li>
                     <li className='pb-1'>Grow the dev community through open-source contributions..</li>
                     </p>
                     
                 </div>
-                <div className='w-[90%] md:w-[45%] mt-5 mb-10 md:mb-5 rounded-lg  bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(249,_115,_22)]'>
+                <div className='w-[90%] md:w-[45%] mt-5 mb-10 md:mb-5 rounded-lg  bg-white dark:bg-slate-900 hover:-translate-y-2 duration-300 hover:shadow-[1px_1px_27px_-4px_rgb(249,_115,_22)]'>
                      {/* inner div */}
                      <div className='flex items-center mt-5 pl-5'>
                         <p className='h-[3rem] w-[3.2rem] bg-orange-500 flex justify-center items-center rounded-md'><i className="fa-solid fa-lightbulb text-[1.7em]"></i></p>
-                        <span className='text-[1.4em] pl-4 text-white/90 font-semibold'>My Philosophy</span>
+                        <span className='text-[1.4em] pl-4 text-black dark:text-white/90 font-semibold'>My Philosophy</span>
                     </div>
                     {/* list */}
-                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-white/50'>
+                    <p className='mt-3 mb-5 pl-10 text-[1.1em] text-black/70 dark:text-white/50'>
                     <li className='pb-1'>Technology should simplify lives, not complicate them.</li>
                     <li className='pb-1'>Great software solves problems, not just writes code.</li>
                     <li className='pb-1'>Continuous learning keeps you ahead in tech.</li>
--- src/components/skills/skill.jsx
+++ src/components/skills/skill.jsx
@@ -22,53 +22,53 @@ import vite from './Vite.js.png'
 function Skill(){
     return(
         <>
-        <div className=" bg-slate-900" id="skills">
+        <div className=" bg-slate-100 dark:bg-slate-900" id="skills">
                 <h1 className="text-center text-indigo-400 text-[2.5em] font-bold pt-10">Skills</h1>
 
                 <div className=" w-[100%] flex justify-center items-center mt-8">
                         {/* inner div */}
                         <div className="w-[100%] sm:w-[75%] flex flex-wrap justify-evenly sm:justify-between pb-5 sm:mb-16">
                             
-                            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group  " id="anchor">
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group  " id="anchor">
                             <div className='h-[50%] w-[50%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%]   bg-orange-200/15 rounded-full flex flex-row justify-center items-center '>
                             <img src={html} alt="" className='h-[80%] sm:h-[90%]  w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>HTML</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>HTML</p>
                             </a>
 
-                            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%]   bg-sky-600/15 rounded-full flex flex-row justify-center items-center '>
                             <img src={css} alt="" className='h-[70%] sm:h-[80%] w-[70%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>CSS</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>CSS</p>
                             </a>
 
-                            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target='_blank' className="h-[8.5rem] sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target='_blank' className="h-[8.5rem] sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%] bg-yellow-400/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={javascriptt} alt="" className=' sm:h-[70%] h-[70%] w-[70%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>JavaScript</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>JavaScript</p>
                             </a>
 
-                            <a href="https://react.dev/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://react.dev/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                             <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%] bg-cyan-600/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={react} alt="" className='h-[90%] rounded-full w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>React</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>React</p>
                             </a>
 
-                            <a href="https://getbootstrap.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://getbootstrap.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%] bg-indigo-600/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={bootstrap} alt="" className=' h-[90%] w-[80%]  '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>Bootstrap</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>Bootstrap</p>
                             </a>
 
-                            <a href="https://tailwindcss.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://tailwindcss.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                             <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%]   bg-cyan-500/10 rounded-full flex flex-row justify-center items-center'>
                             <img src={tailwind} alt="" className='h-[90%] w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>Tailwind</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>Tailwind</p>
                             </a>
 
                             {/* <a href="" className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
@@ -92,11 +92,11 @@ function Skill(){
                             <p className='text-white text-[1.2em] font-semibold'>Express.js</p>
                             </a> */}
 
-                            <a href="https://www.w3schools.com/sql/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://www.w3schools.com/sql/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                             <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%] bg-sky-500/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={sql} alt="" className='h-[90%] w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>SQL</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>SQL</p>
                             </a>
 
                             {/* <a href="" className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
@@ -106,11 +106,11 @@ function Skill(){
                             <p className='text-white text-[1.2em] font-semibold'>MongoDB</p>
                             </a> */}
 
-                            <a href="https://www.w3schools.com/cpp/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://www.w3schools.com/cpp/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%] bg-blue-500/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={cpp} alt="" className='h-[90%] w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>C++</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>C++</p>
                             </a>
 
                             {/* <a href="" className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
@@ -120,25 +120,25 @@ function Skill(){
                             <p className='text-white text-[1.2em] font-semibold'>Python</p>
                             </a> */}
 
-                            <a href="https://code.visualstudio.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://code.visualstudio.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%]   bg-blue-500/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={vscode} alt="" className='h-[80%] sm:h-[90%] w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>VS code</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>VS code</p>
                             </a>
 
-                            <a href="https://git-scm.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://git-scm.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%]   bg-orange-500/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={git} alt="" className='h-[90%] w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>Git</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>Git</p>
                             </a>
 
-                            <a href="https://github.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://github.com/" target='_blank' className="h-[8.5rem]  sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%]  rounded-full flex flex-row justify-center items-center'>
                             <img src={github} alt="" className='h-[90%] w-[80%]  invert  '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>GitHub</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>GitHub</p>
                             </a>
 
                             {/* <a href="" className="h-[8.5rem] sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
@@ -148,11 +148,11 @@ function Skill(){
                             <p className='text-white text-[1.2em] font-semibold'>Postman</p>
                             </a> */}
 
-                            <a href="https://vite.dev/" target='_blank' className="h-[8.5rem] sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
+                            <a href="https://vite.dev/" target='_blank' className="h-[8.5rem] sm:w-[20%] md:w-[18%]  lg:w-[15%] bg-black/20 dark:bg-slate-800 rounded-lg mt-5 ml-2 mr-2 flex flex-col justify-evenly items-center hover:transform hover:-translate-y-2 duration-300 group" id="anchor">
                              <div className='h-[50%] w-[55%] sm:h-[55%] sm:w-[65%] lg:h-[65%] md:w-[55%] bg-indigo-500/15 rounded-full flex flex-row justify-center items-center'>
                             <img src={vite} alt="" className='h-[90%] w-[80%] '/> 
                             </div>
-                            <p className='text-white text-[1.2em] font-semibold'>Vite</p>
+                            <p className='text-black/70 dark:text-white text-[1.2em] font-semibold'>Vite</p>
                             </a>
                             
 
